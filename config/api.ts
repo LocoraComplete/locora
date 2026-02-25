7123eb1867f625040ddac271b1410239f070bfa2
kavya-jais <jaiswal@gmail.com>
2026-02-26 01:49:01 +0530

    Add/update userroutes.js in backend routes

--- config/api.ts
+++ config/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 import Constants from "expo-constants";
 
-export const API_BASE_URL =
-  Constants.expoConfig?.extra?.apiUrl ??
-  "https://locora-backend.onrender.com";
+export const API_BASE_URL ="http://10.146.162.159:5000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
