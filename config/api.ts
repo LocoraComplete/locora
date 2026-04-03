aacd46fd0a9853e2e10a827fc3b61d73588797c2
PalakSinha2505 <palaksinha2505@gmail.com>
2026-04-03 14:10:49 +0530

    Api.ts link

--- config/api.ts
+++ config/api.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
-/*import Constants from "expo-constants";
+import Constants from "expo-constants";
 
 export const API_BASE_URL =
   Constants.expoConfig?.extra?.apiUrl ??
-  "https://locora-backend.onrender.com";*/
+  "https://locora-backend.onrender.com";
 
-export const API_BASE_URL = "http://10.226.228.72:5000"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
